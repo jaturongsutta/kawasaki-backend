7ce2a19c82daa0a44957d7dc8bdb1d1c9d0b2ed0
Note <note_kap@hotmail.com>
2025-04-30 09:42:22 +0700

    Add entity definitions for M_Line, M_Line_Model, M_Line_Machine, and M_Line_Tool

---
+++ src/entity/m-line-machine.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, Index } from 'typeorm';
+
+@Index('PK_M_Line_Machine', ['lineCd', 'modelCd', 'processCd'], {
+  unique: true,
+})
+@Entity('M_Line_Machine', { schema: 'dbo' })
+export class MLineMachine {
+  @Column('nvarchar', { primary: true, name: 'Line_CD', length: 10 })
+  lineCd: string;
+
+  @Column('nvarchar', { primary: true, name: 'Model_CD', length: 10 })
+  modelCd: string;
+
+  @Column('nvarchar', { primary: true, name: 'Process_CD', length: 10 })
+  processCd: string;
+
+  @Column('time', { name: 'WT', nullable: true })
+  wt: Date | null;
+
+  @Column('time', { name: 'HT', nullable: true })
+  ht: Date | null;
+
+  @Column('time', { name: 'MT', nullable: true })
+  mt: Date | null;
+
+  @Column('char', { name: 'is_Active', nullable: true, length: 1 })
+  isActive: string | null;
+
+  @Column('datetime', { name: 'CREATED_DATE', nullable: true })
+  createdDate: Date | null;
+
+  @Column('nvarchar', { name: 'CREATED_BY', nullable: true, length: 10 })
+  createdBy: string | null;
+
+  @Column('datetime', { name: 'UPDATED_DATE', nullable: true })
+  updatedDate: Date | null;
+
+  @Column('nvarchar', { name: 'UPDATED_BY', nullable: true, length: 10 })
+  updatedBy: string | null;
+}
