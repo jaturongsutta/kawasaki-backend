cdc285850e6db7d35d0873e6e401cadfe7cfa29b
Note <note_kap@hotmail.com>
2025-05-07 11:42:07 +0700

    Add PlanSearchDto and implement search functionality in PlanService and PlanController

--- src/api/plan/plan.controller.ts
+++ src/api/plan/plan.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body } from '@nestjs/common';
+import { BaseController } from 'src/base.controller';
+import { PlanService } from './plan.service';
+import { PlanSearchDto } from './dto/plan-search.dto';
 
 @Controller('plan')
-export class PlanController {}
+export class PlanController extends BaseController {
+  constructor(private service: PlanService) {
+    super();
+  }
+
+  // API to get the current plan list by line
+  @Get('plan-list-current/:line')
+  async getPlanListCurrent(@Query('line') line: string) {
+    return await this.service.planListCurrent(line);
+  }
+
+  // API to search plans
+  @Post('search')
+  async searchPlans(@Body() dto: PlanSearchDto) {
+    return await this.service.search(dto);
+  }
+}
--- src/api/plan/plan.service.ts
+++ src/api/plan/plan.service.ts
@@ -3,12 +3,46 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CommonService } from 'src/common/common.service';
 import { ProdPlan } from 'src/entity/prod-plan';
 import { Repository } from 'typeorm';
+import { PlanSearchDto } from './dto/plan-search.dto';
 
 @Injectable()
 export class PlanService {
   constructor(
     @InjectRepository(ProdPlan)
-    private predefineRepository: Repository<ProdPlan>,
+    private planRepository: Repository<ProdPlan>,
     private commonService: CommonService,
   ) {}
+
+  async planListCurrent(line: string) {
+    try {
+      const req = await this.commonService.getConnection();
+      req.input('Line_CD', line);
+      const { recordset } = await this.commonService.executeStoreProcedure(
+        'sp_Plan_List_Current',
+        req,
+      );
+      return recordset;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async search(dto: PlanSearchDto) {
+    try {
+      const req = await this.commonService.getConnection();
+      req.input('Line_CD', dto.line);
+      req.input('Plan_Date_From', dto.dateFrom);
+      req.input('Plan_Date_To', dto.dateTo);
+      req.input('Model_CD', dto.lineModel);
+      req.input('Status', dto.status);
+      req.input('Row_No_From', dto.searchOptions.rowFrom);
+      req.input('Row_No_To', dto.searchOptions.rowTo);
+
+      return await this.commonService.getSearch('sp_Plan_Search', req);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
