5095eda25a8ca3fa5342fdfebba2bb8666d29eb5
Chartchawan <chartchawan.p@gmail.com>
2025-06-05 18:02:46 +0700

    Fix tool add field machineNo

--- src/entity/tool.entity.ts
+++ src/entity/tool.entity.ts
@@ -1,15 +1,18 @@
 import { Column, Entity, Index } from "typeorm";
 
-@Index("PK_M_Tool", ["processCd", "toolCd"], { unique: true })
+@Index("PK_M_Tool", ["processCd", "machineNo","hCode"], { unique: true })
 @Entity("M_Tool", { schema: "dbo" })
 export class MTool {
   @Column("nvarchar", { primary: true, name: "Process_CD", length: 10 })
   processCd: string;
+  
+  @Column("nvarchar", { primary: true, name: "Machine_No", length: 10 })
+  machineNo: string;
 
   @Column("nvarchar", { primary: true, name: "H_Code", length: 10 })
   hCode: string;
 
-  @Column("nvarchar", { primary: true, name: "Tool_CD", length: 10 })
+  @Column("nvarchar", { name: "Tool_CD", length: 10 })
   toolCd: string;
 
   @Column("nvarchar", { name: "Tool_Name", nullable: true, length: 200 })
